ba90b6bdca355c92d20155e2576ac0729dfdd75b
Jeffrey Robinson <deathly809@gmail.com>
2015-12-20 21:58:30 -0600

    Working on stuff, need to commit for other machines to use.

---
+++ main.go
@@ -0,0 +1,25 @@
+//
+//	dfs is a fault tolerant distributed file system.  It allows for auto allocation of resources and
+//  load balancing.
+//
+//
+
+package main
+
+import (
+	"dfs/fs/concrete"
+	"flag"
+	"log"
+)
+
+func main() {
+	isController := flag.Bool("daemon", false, "starts up a controller")
+	if *isController {
+		log.Print("Controller started")
+	}
+	fs,err := concrete.Open("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
